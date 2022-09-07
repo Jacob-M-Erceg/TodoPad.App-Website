55560bd26052ebe7d1afd6040ce60aedfe97c033
Codebrah-youtube <71945422+Codebrah-youtube@users.noreply.github.com>
2022-09-06 19:42:19 -0700

    Support page finished

--- src/components/pages/support/Support.jsx
+++ src/components/pages/support/Support.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './support.css'
 import { useRef } from 'react';
 import emailjs from 'emailjs-com'
@@ -28,17 +28,42 @@ const Support = () => {
         alert("Your support request has been sent.")
     };
 
-    let infoLabel = "Please leave a detailed summary of what you need help with along with your return email address and name. We will get back to you as soon as possible."
+    let infoLabel = "Please leave a detailed summary of what you need help with, along with your return email address and name. We will get back to you as soon as possible."
+
+    const [isMobileScreenSize, setIsMobileScreenSize] = useState(() => {
+        if (window.matchMedia("(max-width: 700px)").matches) {
+            return true
+        } else {
+            return false
+        }
+    })
+
+    useEffect(() => {
+        window.matchMedia("(max-width: 700px)")
+            .addEventListener('change', (e)  => {
+                setIsMobileScreenSize(e.matches)
+            })
+    }, []);
 
     return (
-            <div className="support-page">
+        <div className="support-page">
 
+            { (!isMobileScreenSize == true) &&
                 <div className="left-hand-info-panel">
                     <BackToHomeButton />
                     <label className="page-title-label">Support</label>
                     <label className="page-info-label">{infoLabel}</label>
                 </div>
+            }
 
+            { (isMobileScreenSize == true) &&
+                <div className="left-hand-info-panel">
+                    <label className="page-title-label">Support</label>
+                    <label className="page-info-label">{infoLabel}</label>
+                </div>
+            }
+
+            <div className="support-form-container">
                 <form className="support-form" ref={form} onSubmit={sendEmail}>
                     <ul>
                         <li>
@@ -51,15 +76,15 @@ const Support = () => {
                             <input className="support-form-fields" type="email" name='confirm-email' placeholder='Confirm Email' required />
                         </li>
                         <li>
-                            <textarea className="support-form-fields" name="message" placeholder='What we can help you with' required ></textarea>
+                            <textarea className="support-form-fields" name="message" placeholder='What can we help you with?' required ></textarea>
                         </li>
                         <li>
                             <button type='submit' className='support-form-primary-button'>Send Message</button>
                         </li>
                     </ul>
                 </form>
-
             </div>
+        </div>
     )
 }
 
